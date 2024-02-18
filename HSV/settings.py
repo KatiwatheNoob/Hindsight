b7188f98b9a7ee2eb2230434dde5a013a7ec0c96
Katiwa <roykatiwa@gmail.com>
2024-02-18 23:27:22 +0300

    Commit

--- HSV/settings.py
+++ HSV/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hindsight-test.onrender.com']
+ALLOWED_HOSTS = ['hindsight-ventures.onrender.com']
 
 
 # Application definition
