42420a87c99dc6f00104a3719bc050df4571f2bb
Katiwa <roykatiwa@gmail.com>
2024-02-19 11:18:25 +0300

    Alert

--- HSV/urls.py
+++ HSV/urls.py
@@ -7,7 +7,7 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Hindsight.urls')),
-    path('property/', include('Hindsight.urls')),
+    
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
